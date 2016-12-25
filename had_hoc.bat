be4f61421bfeaf29fb33c3d6899302d1bdd23444
GuyLandry <guy.kenfack@hotmail.com>
2016-12-25 04:20:45 +0100

    Create had_hoc.bat
    With this file you can set an hac_doc wifi with your computer.
    Hac_hoc wifi is suitable for student living cities not covered by wi-fi

---
+++ had_hoc.bat
@@ -0,0 +1,27 @@
+REM how to create a had_hoc wifi on your computer
+
+@echo off
+CLS
+
+:MENU
+echo color 0
+ECHO.
+ECHO ----------------------------------------------------------
+ECHO --- Press 1, 2, or 3 to select your task, or 4 to Exit ---
+ECHO ----------------------------------------------------------
+ECHO.
+ECHO 1 - Set Wifi Sharing Attributes
+ECHO 2 - Start WiFi Sharing
+ECHO 3 - Stop WiFi Sharing
+ECHO 4 - Details
+ECHO 5 - Exit
+ECHO 6 - delete Wifi Sharing
+ECHO.
+
+SET /P M=Type 1, 2, 3, 4, or 5, then press ENTER: 
+IF %M%==1 GOTO SET
+IF %M%==2 GOTO START
+IF %M%==3 GOTO STOP
+IF %M%==4 GOTO DETAILS
+IF %M%==5 GOTO EOF
+IF %M%==6 GOTO DELETE
